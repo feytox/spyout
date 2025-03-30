7bce76709f54abee3132b3a6d37776c55d312bf2
Feytox <73577753+feytox@users.noreply.github.com>
2025-03-30 23:18:39 +0500

    fix: better sprite position anchoring

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -3,15 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteController : MonoBehaviour
 {
-    public float offsetY = 0.35f;
-    private Transform spriteTransform;
     private PlayerController playerController;
-    private Transform bodyTransform;
-
-    void Awake()
-    {
-        spriteTransform = transform;
-    }
 
     void Start()
     {
@@ -19,15 +11,6 @@ public class SpriteController : MonoBehaviour
         Debug.Assert(playerManager != null);
 
         playerController = playerManager.PlayerController;
-        bodyTransform = playerController.BodyTransform;
-    }
-
-    // Not sure that it will work properly cuz theres no latefixedupdate
-    void Update()
-    {
-        var tempPos = bodyTransform.position;
-        tempPos.y += offsetY;
-        tempPos.z = spriteTransform.position.z;
-        spriteTransform.position = tempPos;
+        transform.SetParent(playerController.BodyTransform, false); // i think there's even better solution
     }
 }
