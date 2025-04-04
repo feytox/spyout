ef844ec53dcf2d0d571721ef6530fbe6f627b995
Feytox <73577753+feytox@users.noreply.github.com>
2025-04-05 00:12:27 +0500

    fix: better camera positioning (like in 7bce76)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,28 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-    private float offsetY = 0.35f;
-    private Transform cameraTransform;
-    private Transform bodyTransform;
-
-    void Awake()
-    {
-        cameraTransform = transform;
-    }
-
+    
     void Start()
     {
         var playerManager = GetComponentInParent<PlayerManager>();
         Debug.Assert(playerManager != null);
 
-        bodyTransform = playerManager.PlayerController.BodyTransform;
-    }
-
-    void LateUpdate()
-    {
-        var tempPos = bodyTransform.position;
-        tempPos.y += offsetY;
-        tempPos.z = cameraTransform.position.z;
-        cameraTransform.position = tempPos;
+        transform.SetParent(playerManager.PlayerController.BodyTransform);
     }
 }
