61929d76dcbf5e38a2bea10fcd8df3c4561b716c
graphento <idc>
2025-04-14 23:48:07 +0500

    release: singleton

--- Assets/Scripts/Modules/Singleton.cs
+++ Assets/Scripts/Modules/Singleton.cs
@@ -1,28 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// [DisallowMultipleComponent]
-public class Singleton : MonoBehaviour
+[DisallowMultipleComponent]
+public abstract class Singleton<T> : MonoBehaviour
+    where T : Singleton<T>
 {
-    private static readonly Dictionary<string, Singleton> _singletons = new();
+    protected abstract void Init(); // instead of awake
 
-    protected virtual void Init() { } // instead of awake
+    protected static T _singleton;
 
     void Awake()
     {
-        var isExist = _singletons.TryGetValue(GetType().Name, out var singleton);
         Debug.Assert(
-            !isExist || singleton == null,
-            $"{gameObject.name} tried to awake {GetType().Name} second time!"
+            _singleton == null,
+            $"{gameObject.name} tried to awake {typeof(T).Name} second time!"
         );
-        _singletons[GetType().Name] = this;
+        _singleton = this as T;
         Init();
     }
 
-    protected static Singleton GetInstance<T>()
+    protected static T GetInstance()
     {
-        var singleton = _singletons[nameof(T)];
-        Debug.Assert(singleton != null, $"Tried to access {nameof(T)} before it was initialized!");
-        return singleton;
+        Debug.Assert(
+            _singleton != null,
+            $"Tried to access {typeof(T).Name} before it was initialized!"
+        );
+        return _singleton;
     }
 }
