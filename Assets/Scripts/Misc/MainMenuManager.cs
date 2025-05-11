a612e1656cd886e2f7d1cf32d3395e6c85cedc50
Feytox <73577753+feytox@users.noreply.github.com>
2025-05-11 20:19:01 +0500

    fix: changed main scene

--- Assets/Scripts/Misc/MainMenuManager.cs
+++ Assets/Scripts/Misc/MainMenuManager.cs
@@ -6,7 +6,7 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField]
     private LevelLoader _levelLoader;
     
-    public void OnClickPlay() => _levelLoader.LoadLevel("GeorgeScene");
+    public void OnClickPlay() => _levelLoader.LoadLevel("Town");
 
     public void OnClickQuit() => Application.Quit();
 }
