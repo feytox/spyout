05ab6593ae2eb5e5cc6b89c64478451ef2a107bb
Feytox <73577753+feytox@users.noreply.github.com>
2025-05-25 20:06:23 +0500

    feat: settings (without controls)

--- Assets/Scripts/Sounds/SoundMixerManager.cs
+++ Assets/Scripts/Sounds/SoundMixerManager.cs
@@ -4,4 +4,28 @@ using UnityEngine.Audio;
 public class SoundMixerManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer _mixer;
+
+    public float MasterVolume => GetVolume("masterVolume");
+    public float MusicVolume => GetVolume("musicVolume");
+    public float SoundFXVolume => GetVolume("soundFXVolume");
+
+    public void SetMasterVolume(float percent) => SetVolume("masterVolume", percent);
+
+    public void SetMusicVolume(float percent) => SetVolume("musicVolume", percent);
+
+    public void SetSoundFXVolume(float percent) => SetVolume("soundFXVolume", percent);
+
+    private void SetVolume(string group, float percent)
+    {
+        _mixer.SetFloat(group, Mathf.Log10(percent) * 20f);
+    }
+
+    private float GetVolume(string group)
+    {
+        if (_mixer.GetFloat(group, out var volume))
+            return Mathf.Pow(10, volume / 20f);
+
+        Debug.LogError($"Unknown sound type {group}");
+        return 0;
+    }
 }
---
+++ Assets/Scripts/UI/VolumeSettingComponent.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSettingComponent : MonoBehaviour
+{
+    [SerializeField] private SoundMixerManager _mixerManager;
+    [SerializeField] private VolumeGroup _volumeGroup;
+
+    private Slider _volumeSlider;
+
+    void Start()
+    {
+        _volumeSlider = GetComponentInChildren<Slider>();
+        SetCurrentVolume();
+    }
+
+    public void SetVolume(float percent)
+    {
+        switch (_volumeGroup)
+        {
+            case VolumeGroup.Master:
+                _mixerManager.SetMasterVolume(percent);
+                break;
+            case VolumeGroup.Music:
+                _mixerManager.SetMusicVolume(percent);
+                break;
+            case VolumeGroup.SoundFX:
+                _mixerManager.SetSoundFXVolume(percent);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void SetCurrentVolume()
+    {
+        var percent = _volumeGroup switch
+        {
+            VolumeGroup.Master => _mixerManager.MasterVolume,
+            VolumeGroup.Music => _mixerManager.MusicVolume,
+            VolumeGroup.SoundFX => _mixerManager.SoundFXVolume,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        _volumeSlider.value = percent;
+    }
+}
+
+public enum VolumeGroup : byte
+{
+    Master = 0,
+    Music = 1,
+    SoundFX = 2
+}
