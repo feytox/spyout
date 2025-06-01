7b1d7ce989fee985709cd72cb562827e507fd4ed
Barmin_George <183339070+BarminGeorge@users.noreply.github.com>
2025-06-01 11:58:27 +0500

    не получается эта обводка

--- Assets/Scripts/Cursor/Cursor.cs
+++ Assets/Scripts/Cursor/Cursor.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class CustomCursor : MonoBehaviour
 {
     [SerializeField] private Texture2D cursorTexture;
-    [SerializeField] private Vector2 hotSpot = Vector2.zero;
     [SerializeField] private bool visible = true;
 
     private void Awake()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
         Cursor.visible = visible;
     }
 }
